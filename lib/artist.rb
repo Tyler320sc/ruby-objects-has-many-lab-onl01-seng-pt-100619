a1f843ef055b65fdf511a6f3a21b02af328766ff
Tyler Carroll <tylercarroll1611@gmail.com>
2019-11-06 06:07:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,32 @@
 class Artist
+  attr_accessor :name
+
   @@all = []
-  attr_accessor :name, :songs
+
   def initialize(name)
     @name = name
-    save
+    @@all << self
   end
+
   def self.all
-    @@all    
+    @@all
   end
+
   def add_song(song)
     song.artist = self
   end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
+
   def songs
     Song.all.select {|song| song.artist == self}
   end
-  def self.find_or_create_by_name(name)
-    self.find(name) ? self.find(name) : self.new(name)
-    #this uses a ternary statement, whereas we used an if
-  end
-  def self.find(name) #we called this find_by_name
-    self.all.find {|artist| artist.name == name }
-  end
-  def save
-    @@all << self
-  end
-  def print_songs
-    songs.each {|song| puts song.name}
+
+  def self.song_count
+    Song.all.count
   end
 end
 
